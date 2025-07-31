30db91dadde038ee16cac2dea3622f62e552341b
DhikaV0 <167159222+DhikaV0@users.noreply.github.com>
2025-07-31 08:02:53 +0700

    add icon

--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+
 import StatusCard from "../status-card";
 import QuickActions from "../quick-actions";
 import Chart from "../chart";
@@ -7,9 +9,9 @@ export default function Dashboard() {
     return (
         <div className="min-h-screen p-6 space-y-6">
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
-                <StatusCard name="Total Books" value={10} />
-                <StatusCard name="Active Borrowings" value={5} />
-                <StatusCard name="Pending Fines" value={5} />
+                <StatusCard name="Total Books" value={10} icon={faBook} />
+                <StatusCard name="Active Borrowings" value={5} icon={faBook} />
+                <StatusCard name="Pending Fines" value={5} icon={faBook} />
             </div>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
--- src/components/status-card.tsx
+++ src/components/status-card.tsx
@@ -1,14 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 
 interface Prop {
     name: string,
     value: number,
+    icon: IconDefinition
 }
 
-export default function StatusCard({name, value}: Prop) {
+export default function StatusCard({name, value, icon}: Prop) {
     return (
         <div className="flex items-center justify-between p-8 rounded-lg bg-white gap-8 shadow-md">
-            <div className="p-6 rounded-full bg-[var(--main-theme)]/20"></div>
+            <div className="p-6 rounded-full bg-[var(--main-theme)]/20">
+                <FontAwesomeIcon icon={icon} className="text-black" />
+            </div>
             <div>
                 <h1 className="text-xl text-gray-700 font-bold">{value}</h1>
                 <p className="text-gray-400">{name}</p>
